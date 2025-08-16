e10ea657b76bdf277024ad96a682267fb5cb0bde
A4BHI <manthukafc@gmail.com>
2025-08-16 18:50:04 +0530

    updated sessionchecker

--- go-backend/sessionchecker/check.go
+++ go-backend/sessionchecker/check.go
@@ -1,9 +1,12 @@
 package sessionchecker
 
 import (
+	"context"
+	db "echoai/db"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 func Check(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +18,20 @@ func Check(w http.ResponseWriter, r *http.Request) {
 
 		var sessionid string = string(body)
 		fmt.Println(sessionid)
+		conn, err := db.Connectdb()
+
+		if err != nil {
+			fmt.Println("Error From sessionchecker [ Error Connecting to DB ]", err)
+
+		}
+		var expiry time.Time
+		conn.QueryRow(context.Background(), "select expiry_time from sessions where sessionid=$1", sessionid).Scan(expiry)
+
+		if expiry.After(time.Now()) {
+			fmt.Fprintf(w, "Success")
+		} else {
+			fmt.Fprintf(w, "fail")
+		}
 
 	}
 
